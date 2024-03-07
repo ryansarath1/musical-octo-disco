d4a710d66b4503616a923ed9e4904e1d1634ba4e
sarathryan@yahoo.com <sarathryan@yahoo.com>
2024-03-06 17:15:06 -0800

    update on Readme file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# musical-octo-disco
+10 Object-oriented Programming: SVG Logo Maker
+
+My task is to build a Node.js command-line application that takes in user input to generate a logo and save it as an SVG file. The application prompts the user to select a color and shape, provide text for the logo, and save the generated SVG to a .svg file.
+
+
+*Installation
+npm inquirer version@8.2.4
+
+*User Story
+"AS a freelance web developer
+I WANT to generate a simple logo for my projects
+SO THAT I don't have to pay a graphic designer"
--- index.js
+++ index.js
@@ -1,8 +1,51 @@
 const inquirer = require("inquirer")
 const fs = require("fs")
-
+const SVG = require("./lib/SVG")
+const {Circle, Triangle, Square} =require("./lib/shape")
 
 function askUser() {
+    inquirer.prompt([
+        {
+            type:"input",
+            name:"text",
+            message:"enter 3 letters",
+        },
+        {
+            type:"input",
+            name:"textcolor",
+            message:"enter color for text",
+        },
+        {
+            type:"input",
+            name:"backgroundcolor",
+            message:"What color would you like your shape",
+        },
+        {
+            type:"list",
+            name:"shape",
+            message:"choose a shape",
+            choices:["Circle", "Triangle", "Square"],
+        },
+    ]).then((promptanswers)=>{
+        let choosingshape;
+        switch(promptanswers.shape){
+            case "Circle":
+                choosingshape=new Circle();
+            break;
+            case "Triangle":
+                choosingshape=new Triangle();
+            break;
+            case "Square":
+                choosingshape=new Square();
+            break;    
+
+        }
+        choosingshape.setColor(promptanswers.backgroundcolor)
+        const myshape=new SVG()
+        myshape.setShape(choosingshape)
+        myshape.settext(promptanswers.text,promptanswers.textcolor)
+        promptanswers.length>3?console.log("must be 3 characters or less"): fs.writeFileSync("myshape.svg", myshape.render())
+    })
     
 
 }
